92b02b093f4de4d6c619b62f545d0262594ec202
dapt <diegodapt18@gmail.com>
2023-09-15 10:45:50 +0200

    editing the estore and the HomeView.vue

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,86 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    questions: [
+      {
+        title: 'Dawn or dusk?',
+        answers: [
+          {
+            title: 'Dawn',
+            scores: {
+              g: 100,
+              r: 100,
+              h: 0,
+              s: 0
+            }
+          },
+          {
+            title: 'Dusk',
+            scores: {
+              g: 0,
+              r: 0,
+              h: 100,
+              s: 100
+            }
+          }
+        ]
+      },
+      {
+        title: 'Forest or river?',
+        answers: [
+          {
+            title: 'Forest',
+            scores: {
+              g: 100,
+              r: 100,
+              h: 0,
+              s: 0
+            }
+          },
+          {
+            title: 'River',
+            scores: {
+              g: 0,
+              r: 0,
+              h: 100,
+              s: 100
+            }
+          }
+        ]
+      }
+    ],
+    scores: {
+      g: 0,
+      h: 0,
+      r: 0,
+      s: 0
+    }
   },
   getters: {
+    getQuestionByIndex (state) {
+      return (index) => state.questions[index]
+    },
+    getScores (state) {
+      return state.scores
+    },
+    getHighestScore (state) {
+      const max = Math.max(...Object.values(state.scores))
+      for (const key in state.scores) {
+        if (state.scores[key] === max) {
+          return key
+        }
+      }
+    }
   },
   mutations: {
+    storeQuestions (state, questions) {
+      state.questions = questions
+    },
+    setScores (state, scores) {
+      for (const key in scores) {
+        state.scores[key] += scores[key]
+      }
+    }
   },
   actions: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,66 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <ul>
+    <li v-for="(value, key) in scoresObject" :key="key">{{key}}: {{value}}</li>
+  </ul>
+  <button @click="addPoints">add points</button>
+  <div v-if="winner">Winner: {{winner}}</div>
+  <button @click="getWinner">get winner</button>
+  <ul>
+    <li v-for="(value, index) of questionsObject" :key="index">{{value.title}}</li>
+  </ul>
+
+  <button @click="addQuestion">add question</button>
 </template>
 
-<script>
+<script setup>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import store from '@/store'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+const scoresObject = ref(store.getters.getScores)
+const questionsObject = ref([])
+const winner = ref('')
+
+const addPoints = () => {
+  const scores = {
+    g: Math.floor(Math.random() * 10),
+    h: Math.floor(Math.random() * 10),
+    r: Math.floor(Math.random() * 10),
+    s: Math.floor(Math.random() * 10)
+  }
+  store.commit('setScores', scores)
+}
+
+const getWinner = () => {
+  winner.value = store.getters.getHighestScore
+}
+
+function * getIndex () {
+  let index = 0
+
+  while (true) {
+    yield index++
   }
 }
+
+const generator = getIndex()
+
+const addQuestion = () => {
+  questionsObject.value.push(store.getters.getQuestionByIndex(generator.next().value))
+}
+
+const getQuestionData = async () => {
+  const url = 'https://gist.githubusercontent.com/Xowap/b01fbce0c23971e23ab28427c09f92b3/raw/ad6399bd659679ff1f3209c2549a359d9cb744fd/sorting_hat.json'
+  const res = await fetch(url)
+  const json = await res.json()
+  store.commit('storeQuestions', json)
+}
+
+onMounted(async () => {
+  await getQuestionData()
+})
+
 </script>
